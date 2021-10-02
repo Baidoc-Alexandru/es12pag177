d3eca209610c6c72ee0f7f7c3715aee90fcfd399
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-02 21:43:55 +0000

    Es12Pag177

---
+++ src/Es12Pag177.java
@@ -0,0 +1,12 @@
+public class Es12Pag177 {
+    public static void main(String[] args) {
+        //inserire valore del lato
+        int lato;
+        //calcolare perimetro del quadrato
+        System.out.println(lato*4);
+        //calcolare area del quadrato
+        System.out.println(lato*lato);
+
+
+    }
+}
